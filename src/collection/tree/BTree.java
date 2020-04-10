dc389d0b365a7ea6f2218a67bf6a0abb616ab482
AlphaLay <794311913@qq.com>
2020-04-10 18:43:45 +0800

    btree enrich

--- src/collection/tree/BTree.java
+++ src/collection/tree/BTree.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.swing.tree.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -20,8 +21,7 @@ public class BTree {
         //afterOrder(bTree.getRoot());
         System.out.println(level(bTree.getRoot(), 0));
         Stack<Integer> stack = new Stack<>();
-        //leftview(bTree.root, stack, 0);
-        leftView2(bTree.root, 0, stack);
+        leftView(bTree.root);
         System.out.println(stack);
         posOrder(bTree.root);
         System.out.println();
@@ -55,16 +55,6 @@ public class BTree {
         bTree.getRoot().getRight().getRight().setRight(new BTreeNode(25));
         bTree.getRoot().getRight().getRight().getRight().setLeft(new BTreeNode(80));
         return bTree;
-    }
-
-    public static void leftView2(BTreeNode node, int level, Stack<Integer> stack) {
-        if (node == null)
-            return;
-        if (stack.size() == level) {
-            stack.push(node.getData());
-        }
-        leftView2(node.getLeft(), level + 1, stack);
-        leftView2(node.getRight(), level + 1, stack);
     }
 
     //先序遍历
@@ -192,32 +182,50 @@ public class BTree {
         mirrorRecursively(node.getRight());
     }
 
+    //求二叉树的深度
     public static int level(BTreeNode node, int level) {
         if (node == null)
             return level;
         return Math.max(level(node.getLeft(), level++), level(node.getRight(), level++));
     }
 
+    public static void leftView(BTreeNode node) {
+        Stack<Integer> stack = new Stack<>();
+        leftView(node, stack, 0);
+        System.out.println("左视图");
+        for (Integer num : stack) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void rightView(BTreeNode node) {
+        Stack<Integer> stack = new Stack<>();
+        rightView(node, stack, 0);
+        System.out.println("右视图");
+        for (Integer num : stack) {
+            System.out.print(num + " ");
+        }
+    }
+
     //打印左视图
-    public static void leftview(BTreeNode root, Stack<Integer> stack, int level) {
+    public static void leftView(BTreeNode root, Stack<Integer> stack, int level) {
         if (root == null)
             return;
         if (level == stack.size()) {//判断是不是每一层的第一个节点
             stack.push(root.getData());
         }
-        leftview(root.getLeft(), stack, level + 1);
-        leftview(root.getRight(), stack, level + 1);
+        leftView(root.getLeft(), stack, level + 1);
+        leftView(root.getRight(), stack, level + 1);
     }
 
     //打印右视图
-    public static void rightview(BTreeNode root, Stack<Integer> stack, int level) {
+    public static void rightView(BTreeNode root, Stack<Integer> stack, int level) {
         if (root == null)
             return;
         if (level == stack.size()) {//判断是不是每一层的第一个节点
             stack.push(root.getData());
         }
-        rightview(root.getRight(), stack, level + 1);
-        rightview(root.getLeft(), stack, level + 1);
+        rightView(root.getRight(), stack, level + 1);
+        rightView(root.getLeft(), stack, level + 1);
     }
 
     //打印从根节点开始和为K的所有节点
@@ -270,4 +278,93 @@ public class BTree {
         return true;
     }
 
+
+}
+
+//树中的节点必须不相同
+class Question6 {
+    private static boolean flag = false;  //数据正确与错误的标志,true代表数据错误
+
+    //定义一个内部节点类
+    public static class Node<E> {
+        public Node left;
+        public Node right;
+        public E e;
+
+        public Node(E e) {
+            this.e = e;
+        }
+    }
+
+    /**
+     * 根据先序遍历和中序遍历构建二叉树,并返回跟节点
+     *
+     * @param preOrder 先序遍历数据存储的数组
+     * @param preStart 先序遍历数组的起始位置
+     * @param preEnd   先序遍历数组的结束位置
+     * @param midOrder 中序遍历数据存储的数组
+     * @param midStart 中序遍历数组的起始位置
+     * @param midEnd   中序遍历数组的结束位置
+     * @return
+     */
+    public static Node<Integer> reBuild_BinaryTree(int[] preOrder, int preStart, int preEnd, int[] midOrder, int midStart, int midEnd) {
+        //增加鲁棒性
+        if (preOrder == null || midOrder == null)
+            throw new RuntimeException("Null point Exception");
+        if (preOrder.length != midOrder.length || preOrder.length == 0 || midOrder.length == 0)
+            throw new RuntimeException("date not correct");
+        //递归结束标志,如果大于则说明数组越界了,空节点,直接返回
+        if (midStart > midEnd && preStart > preEnd) return null;
+        //等于说明遍历到了最后一个节点,构建节点返回
+        if (midStart == midEnd && preStart == preEnd) {
+            //此时midStart和preStart理应是同一个数字,但是如果不是呢?那就说明数据不正确
+            if (preOrder[preStart] != midOrder[midStart]) {
+                System.out.println("该数据不是一个正确的二叉树的先序遍历和中序遍历");
+                flag = true; //
+                return null;
+            }
+            return new Node(midOrder[midEnd]);
+        }
+        int mid;//指向中序遍历中每一次的跟节点.
+        int pre;//指向先序遍历中左子树的结束位置
+        //找出中序遍历中根节点的位置
+        for (mid = midStart; mid < midEnd; mid++) {
+            if (midOrder[mid] == preOrder[preStart]) break;
+        }
+        //如果输入的数据不正确,应该输出提示
+        if (mid == midOrder.length) {
+            System.out.println("该数据不是一个正确的二叉树的先序遍历和中序遍历");
+            flag = true; //
+            return null;
+        }
+        Node node = new Node(preOrder[preStart]);
+        //找出先序遍历中左子树的结束位置
+        pre = preStart + (mid - midStart);
+        //递归构建二叉树,这里很容易出错,要注意
+        //preOrder的左子树:preStart+1,pre  右子树:pre+1,preEnd;
+        //midOrder的左子树:midStart,mid-1     右子树:mid+1,midEnd
+        node.left = reBuild_BinaryTree(preOrder, preStart + 1, pre, midOrder, midStart, mid - 1);
+        node.right = reBuild_BinaryTree(preOrder, pre + 1, preEnd, midOrder, mid + 1, midEnd);
+        return node;
+    }
+
+    /**
+     * 后序遍历输出二叉树
+     *
+     * @param head
+     */
+    public static void postOrder_print(Node head) {
+        if (flag) return;//数据错误,直接返回
+        if (head == null) return;
+        postOrder_print(head.left);
+        postOrder_print(head.right);
+        System.out.println(head.e);
+    }
+
+    public static void main(String[] args) {
+        int[] preOrder = {1, 3, 5, 7};
+        int[] midOrder = {3, 1, 7, 5};
+        Node<Integer> head = reBuild_BinaryTree(preOrder, 0, preOrder.length - 1, midOrder, 0, midOrder.length - 1);
+        postOrder_print(head);
+    }
 }
--- src/collection/tree/BTreeNode.java
+++ src/collection/tree/BTreeNode.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Stack;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,6 +18,64 @@ public class BTreeNode {
         this.data = data;
     }
 
+    public void add(Integer v) {
+        // 如果当前节点没有值，就把数据放在当前节点上
+        if (null == data)
+            data = v;
 
+            // 如果当前节点有值，就进行判断，新增的值与当前值的大小关系
+        else {
+            // 新增的值，比当前值小或者相同
+
+            if (v - data <= 0) {
+                if (null == left)
+                    left = new BTreeNode();
+                left.add(v);
+            }
+            // 新增的值，比当前值大
+            else {
+                if (null == right)
+                    right = new BTreeNode();
+                right.add(v);
+            }
+
+        }
+
+    }
+
+    public static void add(BTreeNode node, Integer v) {
+        // 如果当前节点没有值，就把数据放在当前节点上
+        if (null == node.getData())
+            node.setData(v);
+            // 如果当前节点有值，就进行判断，新增的值与当前值的大小关系
+        else {
+            // 新增的值，比当前值小或者相同
+            if (v - node.getData() <= 0) {
+                if (null == node.getLeft())
+                    node.setLeft(new BTreeNode());
+                add(node.getLeft(), v);
+            }
+            // 新增的值，比当前值大
+            else {
+                if (null == node.getRight())
+                    node.setRight(new BTreeNode());
+                add(node.getRight(), v);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BTreeNode node = new BTreeNode(15);
+        add(node, 12);
+        add(node, 17);
+        add(node, 20);
+        add(node, 5);
+        add(node, 14);
+        BTree.levelOrder(node);
+        BTree.leftView(node);
+        BTree.rightView(node);
+    }
 
 }
